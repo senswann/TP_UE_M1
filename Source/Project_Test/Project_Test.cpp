550daebd2d38f80f5850389ea7b3dab380f83fbd
senswann <84702005+senswann@users.noreply.github.com>
2024-01-15 13:51:09 +0100

    init

---
+++ Source/Project_Test/Project_Test.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Project_Test.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Project_Test, "Project_Test" );
+ 
---
+++ Source/Project_Test/Project_TestGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Project_TestGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AProject_TestGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AProject_TestGameMode();
+};
+
+
+
